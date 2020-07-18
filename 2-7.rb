f2d858ccc64357ddd05b957f0d2e5d1d23ae5921
nelml <konpaku.youmi@gmail.com>
2020-07-18 13:17:46 +0900

    [FINISH]2-7

---
+++ 2-7.rb
@@ -0,0 +1,24 @@
+apple = 110
+if apple >= 100
+	puts "リンゴの値段は100円以上です。"
+end
+
+if apple <= 100
+	puts "リンゴの値段は100円以下です。"
+end
+
+tall = 170
+
+if tall >= 170 && tall <=190
+	puts "身長は170以上、190以下です。"
+end
+
+fuji = "Aomori"
+
+if fuji == "Nagano"
+	puts "長野県産や！"
+elsif fuji == "Aomori"
+	puts "青森県産やんけ！"
+else
+	puts "山形のリンゴはおいしいんご"
+end
