75c76f06468bae48552efa732b986ea0651841cc
nelml <konpaku.youmi@gmail.com>
2020-07-18 12:56:34 +0900

    [FINISH]2-6

---
+++ 2-6.rb
@@ -0,0 +1,13 @@
+puts 101 % 25
+
+name = "鈴木さん"
+puts "こんにちは、#{name}"
+
+names = ["Peal", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {"太郎"=>180, "次郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
+length ={:太郎=>180, :次郎=>170, :花子=>150}
+puts length[:太郎]
