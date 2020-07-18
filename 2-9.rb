04250d12fb141a0811dd1c9e9457db724bfbc908
nelml <konpaku.youmi@gmail.com>
2020-07-18 14:24:26 +0900

    [FINISH]2-9

---
+++ 2-9.rb
@@ -0,0 +1,16 @@
+#def hello_world
+#	'Hello, World!'
+#end
+
+def hello_world(name)
+	puts "hello, #{name}"
+end
+
+puts hello_world("taro")
+
+def greeting
+	return "hello, world"
+	"hi, John"
+end
+
+puts greeting
